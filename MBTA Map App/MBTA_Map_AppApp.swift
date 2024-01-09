7de5858fec7417763625a1cfe5c6fe07e4128de9
Chad Wishner <chadwishner@gmail.com>
2024-01-09 13:44:48 -0500

    Initial Commit

---
+++ MBTA Map App/MBTA_Map_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MBTA_Map_AppApp.swift
+//  MBTA Map App
+//
+//  Created by Chad Wishner on 1/9/24.
+//
+
+import SwiftUI
+
+@main
+struct MBTA_Map_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
